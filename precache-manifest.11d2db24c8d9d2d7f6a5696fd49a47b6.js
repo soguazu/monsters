1a72b7845b20c302c9c0ffe2796fa58c5e35e664
dearests <soguazu@gmail.com>
2019-08-17 07:43:33 +0100

    Updates

---
+++ precache-manifest.11d2db24c8d9d2d7f6a5696fd49a47b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f837a6233bc7ac761cac25d2a9d435c",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "418add1fe5270c9b71b4",
+    "url": "/monsters/static/css/main.ac2c20cc.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "418add1fe5270c9b71b4",
+    "url": "/monsters/static/js/main.523afe08.chunk.js"
+  },
+  {
+    "revision": "0b202f8c0ed788dad152",
+    "url": "/monsters/static/js/runtime~main.39cb6628.js"
+  }
+]);
